7bb986d2b9c472ec764b792145b5bd5b78495d82
Ed Vidal Jr <devel@mypi3-20.attlocal.net>
2020-07-15 05:54:39 -0600

    https://www.raspberrypi.org/forums/viewtopic.php?t=247586 provides instructions Now the libnanovg.a is created the examples are failing using build_examples.sh

---
+++ build_examples.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#build_examples.sh : located in nanovg root directory
+mkdir -p build/examples && cd build/examples
+
+for entry in `ls ../../example/*.c`; do
+    entry_name=`echo "$entry" | awk -F"/" '{print substr($0, length($0)+1-length($NF))}' | rev | awk -F"." '{print substr($0,length($0)+1-length($NF))}' | rev`
+    #echo "$entry_name"
+    gcc "$entry" ../../example/demo.c ../../example/perf.c -I../../example -I../../src -L../../ -L.. -lnanovg -lm -lEGL -lGL -lglfw3 -o "$entry_name"
+done
+
