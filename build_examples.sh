f93a389b231e166487869a0d103aea00d1ac41f9
Ed Vidal Jr <devel@mypi3-20.attlocal.net>
2020-08-08 18:42:40 -0600

    now the examples get built with only 2 working example/example_gles2 example/example_gl2

--- build_examples.sh
+++ build_examples.sh
@@ -5,6 +5,6 @@ mkdir -p build/examples && cd build/examples
 for entry in `ls ../../example/*.c`; do
     entry_name=`echo "$entry" | awk -F"/" '{print substr($0, length($0)+1-length($NF))}' | rev | awk -F"." '{print substr($0,length($0)+1-length($NF))}' | rev`
     #echo "$entry_name"
-    gcc "$entry" ../../example/demo.c ../../example/perf.c -I../../example -I../../src -L../../ -L.. -lnanovg -lm -lEGL -lGL -lglfw3 -o "$entry_name"
+    gcc "$entry" ../../example/demo.c ../../example/perf.c -I../../example -I../../src -L../../ -L.. -lnanovg -lm -lEGL -lGL -lglfw3  -lXxf86vm -lXrandr -lpthread -lXi -ldl -o "$entry_name"
 done
 
